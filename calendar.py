ef67b827a700c8ce281c841afb3f517d92b0df70
naomitr <52180609+naomitr@users.noreply.github.com>
2019-07-09 15:46:31 -0700

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,6 @@
+import calendar
+# calendar.setfirstweekday(calendar.FRIDAY)
+# the_day = calendar.weekday(2002, 7, 19)
+# print(the_day)
+calendar = calendar.calendar(2019, w=2, l=1, c=6, m=3)
+print(calendar)
